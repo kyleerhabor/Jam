ea74ac6c269dfcad888b9d6f4f1df1acff3333a6
Kyle Erhabor <kyleerhabor@gmail.com>
2023-11-11 17:42:45 -0500

    Initial Commit

---
+++ Jam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jam
+//
+//  Created by Kyle Erhabor on 11/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jam/JamApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamApp.swift
+//  Jam
+//
+//  Created by Kyle Erhabor on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct JamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
